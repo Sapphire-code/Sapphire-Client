8f9c5c531d55c3687e863663d208fd03f63d86f8
Sapphire-code <wasimpadho@gmail.com>
2022-10-01 06:02:30 +0000

    removed js code to test css injection

--- src/main.js
+++ src/main.js
@@ -1,7 +1,1 @@
 const { invoke } = window.__TAURI__.tauri;
-
-window.onload = function() {
-    let insertCss = document.getElementById("KrunkerFrame");
-    let inject = insertCss.contentDocument;
-    inject.body.innerHTML = inject.body.innerHTML + '<style>body {Display: none !important;}</style>'
-}
