2aeded2bcf7ccff6e9269544be0d1d7236fc45d1
Sapphire-code <wasimpadho@gmail.com>
2022-10-01 05:51:23 +0000

    testing css injection into the client

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 const { invoke } = window.__TAURI__.tauri;
+
+window.onload = function() {
+    let insertCss = document.getElementById("KrunkerFrame");
+    let inject = insertCss.contentDocument;
+    inject.body.innerHTML = inject.body.innerHTML + '<style>body {Display: none !important;}</style>'
+}
